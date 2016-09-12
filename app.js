4852e930ec2c209f8594ad1c92705d3a6f91115c
robinsoncol <robinsoncol@users.noreply.github.com>
2016-09-12 17:17:03 -0400

    Let Gulp handle Stylus to CSS compilation

--- app.js
+++ app.js
@@ -20,8 +20,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Processing of stylus file is now handled through gulp
+//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
