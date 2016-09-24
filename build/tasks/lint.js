2528e2b540fcf633e2624626f6dd191d18b3cdf2
robinsoncol <robinsoncol@users.noreply.github.com>
2016-09-24 00:07:53 -0400

    Ignore generated js files when linting

--- build/tasks/lint.js
+++ build/tasks/lint.js
@@ -4,21 +4,24 @@ var pugLinter = require('gulp-pug-linter');
 var stylint = require('gulp-stylint');
 var paths = require('../paths');
 
-gulp.task('lintjs', function () {
-  return gulp.src(['**/*.js', '!node_modules/**'])
-    .pipe(eslint())
+gulp.task('lint:js', function () {
+  return gulp.src([
+    '**/*.js',
+    '!node_modules/**',
+    '!test/coverage/**'
+  ]).pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('lintpug', function () {
+gulp.task('lint:pug', function () {
   return gulp
     .src(paths.views)
     .pipe(pugLinter())
     .pipe(pugLinter.reporter('fail'));
 });
 
-gulp.task('lintstyl', function () {
+gulp.task('lint:stylus', function () {
   return gulp.src(paths.stylus)
     .pipe(stylint())
     .pipe(stylint.reporter())
