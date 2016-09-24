45f8473c2e27239f58243225bf76cc8cd205a136
robinsoncol <robinsoncol@users.noreply.github.com>
2016-09-24 00:06:19 -0400

    Add paths for testing

--- build/paths.js
+++ build/paths.js
@@ -1,11 +1,15 @@
 var app = 'app.js';
-var routes = 'routes/**/*.js';
 var views = 'views/**/*.pug';
+var controllers = 'controllers/**/*.js';
 var stylus = 'assets/css/**/*.styl';
+var test = 'test/unit/**/*.test.js';
+var e2eLib = 'test/e2e/lib';
+
 module.exports = {
   // Files paths in sources should trigger a browser refresh upon changes made to them
-  sources: [app, routes, views, stylus],
+  sources: [app, controllers, views, stylus],
   views: views,
   stylus: stylus,
-  outputs: ['builtAssets/']
+  test: test,
+  e2eLib: e2eLib
 };
