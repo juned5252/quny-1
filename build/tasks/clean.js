63a57243f06ac88c2789450a672d36a8a35f3bd6
robinsoncol <robinsoncol@users.noreply.github.com>
2016-09-24 00:07:15 -0400

    Clean up file generated by testing

--- build/tasks/clean.js
+++ build/tasks/clean.js
@@ -3,8 +3,17 @@ var del = require('del');
 var vinylPaths = require('vinyl-paths');
 var paths = require('../paths');
 
-// Deletes all files in the output path
-gulp.task('clean', function() {
-  return gulp.src(paths.outputs)
+gulp.task('clean:assets', function() {
+  return gulp.src('builtAssets/')
+    .pipe(vinylPaths(del));
+});
+
+gulp.task('clean:coverage', function() {
+  return gulp.src('test/coverage')
+    .pipe(vinylPaths(del));
+});
+
+gulp.task('clean:lib', function() {
+  return gulp.src(paths.e2eLib)
     .pipe(vinylPaths(del));
 });
