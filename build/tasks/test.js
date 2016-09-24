4aa8c7aff5f719e9df49d0b20fe56c09d8878f32
robinsoncol <robinsoncol@users.noreply.github.com>
2016-09-24 00:50:26 -0400

    Comment gulp tasks

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -5,16 +5,19 @@ var configs = require('../configs');
 var nightwatch = require('gulp-nightwatch');
 var selenium = require('selenium-download');
 
+// Run unit tests with Mocha
 gulp.task('test', function() {
   return gulp.src(paths.test, {read: false})
     .pipe(mocha(configs.mocha));
 });
 
+// Run unit tests with Mocha and generate coverage using Istanbul
 gulp.task('test:coverage', function() {
   return gulp.src(paths.test, {read: false})
     .pipe(mocha(configs.mochaIstanbul));
 });
 
+// Run end-to-end tests with Nightwatch
 gulp.task('e2e', function() {
   return gulp.src('')
     .pipe(nightwatch({
@@ -22,6 +25,7 @@ gulp.task('e2e', function() {
     }));
 });
 
+// Run end-to-end test with Nightwatch, chrome version
 gulp.task('e2e:chrome', function() {
   return gulp.src('')
     .pipe(nightwatch({
@@ -30,6 +34,8 @@ gulp.task('e2e:chrome', function() {
     }));
 });
 
+// Download selenium standalone driver and chrome web driver
+// Files are downloaded under test/e2e/lib
 gulp.task('e2e:drivers', function() {
   return new Promise(function(resolve) {
     selenium.ensure(paths.e2eLib, function() {
@@ -38,6 +44,8 @@ gulp.task('e2e:drivers', function() {
   });
 });
 
+// Update the selenium standalone driver and chrome web driver
+// Latest versions are placed under test/e2e/lib, overing the previous verions
 gulp.task('e2e:drivers:update', function() {
   return new Promise(function(resolve) {
     selenium.update(paths.e2eLib, function() {
--- build/tasks/watch.js
+++ build/tasks/watch.js
@@ -4,16 +4,19 @@ var bs = require('browser-sync').create('Server');
 var configs = require('../configs');
 var paths = require('../paths');
 
+// Start a server with Browser Sync functionality
 gulp.task('watch', ['server:start'], function () {
   gulp.watch(paths.sources, ['server:restart']);
 });
 
-gulp.task('server:start', [], function () {
+// Start a server. This task is used with the watch task above
+gulp.task('server:start', function () {
   server.listen(configs.server, function (error) {
     if (!error) { bs.init(configs.bs); }
   });
 });
 
+// Restart a server. This task is used with the watch task above
 gulp.task('server:restart', function () {
   server.restart(function (error) {
     if (!error) { bs.reload(); }
