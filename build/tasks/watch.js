43af73afb379dbc08f2d34fdd1873fcae61157ee
robinsoncol <robinsoncol@users.noreply.github.com>
2016-09-25 02:10:49 -0400

    Minor changes

--- build/tasks/watch.js
+++ build/tasks/watch.js
@@ -1,24 +1,28 @@
-var gulp = require('gulp');
-var server = require('gulp-develop-server');
-var bs = require('browser-sync').create('Server');
+var gulp    = require('gulp');
+var server  = require('gulp-develop-server');
+var bs      = require('browser-sync').create('Server');
 var configs = require('../configs');
-var paths = require('../paths');
+var paths   = require('../paths');
 
-// Start a server with Browser Sync functionality
+/** Starts a server and reloads it when files change */
 gulp.task('watch', ['server:start'], function () {
-  gulp.watch(paths.sources, ['server:restart']);
+  gulp.watch(paths.sources,['server:restart']);
 });
 
-// Start a server. This task is used with the watch task above
+/** Starts a server with BrowserSync functionality */
 gulp.task('server:start', function () {
   server.listen(configs.server, function (error) {
-    if (!error) { bs.init(configs.bs); }
+    if (!error) {
+      bs.init(configs.browserSync);
+    }
   });
 });
 
-// Restart a server. This task is used with the watch task above
+/** Restarts the server */
 gulp.task('server:restart', function () {
   server.restart(function (error) {
-    if (!error) { bs.reload(); }
+    if (!error) {
+      bs.reload();
+    }
   });
 });
