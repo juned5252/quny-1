6d05e4ac88edd833d75d61562e272bebc0c656ba
robinsoncol <robinsoncol@users.noreply.github.com>
2016-09-28 08:32:16 -0400

    Use connection strings to connect to database

--- models/index.js
+++ models/index.js
@@ -5,24 +5,19 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + 'config.json')[env];
+var config    = require(__dirname + '/config.json')[env];
 var db        = {};
 
-var sequelize;
+var dbGlobals = (env === 'development') ? { logging: true } : {};
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+var sequelize = new Sequelize(process.env[config.conn_str], dbGlobals);
 
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename);
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(function(file) {
-    if (file.slice(-3) !== '.js') return;
     var model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
