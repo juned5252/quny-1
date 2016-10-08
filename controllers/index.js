ec4f7456320b133bd7cb92f035aee6603f2f3887
mhasan4 <mahmudbd90@gmail.com>
2016-10-08 19:33:59 -0400

    Move signup-route to user-controller

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET index page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Friendly Stuff'});
+router.get('/', function (req, res) {
+  res.render('index', {
+    title: 'Friendly Stuff',
+    user: req.user
+  });
 });
 
-/* GET index page. */
-router.get('/signup', function(req, res) {
-  res.render('signup', { title: 'Sign Up'});
-});
 
 module.exports = router;
